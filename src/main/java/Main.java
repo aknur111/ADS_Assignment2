43f247867bba6399c1f1b974395dd07470307a8a
etoevelina <evelina.penkova0@gmail.com>
2025-10-01 20:00:47 +0500

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,6 @@ import insertionsort.InsertionSort;
 public class Main {
     public static void main(String[] args) {
         int[] data = {12, 11, 13, 5, 6};
-        var res = InsertionSort.sort(data);
         System.out.println("sorted = " + Arrays.toString(data));
-        System.out.println(res);
     }
 }
